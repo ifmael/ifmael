f74ec3d658c380ab69b5c4556e705bec99abfd98
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2020-05-10 20:12:47 +0200

    feat(web): Add query for get blog post

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,7 +1,71 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+async function createBlogPostPages(graphql, actions) {
+  try {
+    const {
+      createPage
+    } = actions;
 
-// You can delete this file if you're not using it
+    const postQuery = `{
+      allSanityPost {
+        nodes {
+          _id
+          _createdAt
+          _rawBody
+          title {
+            en
+            es
+          }
+          tags {
+            title {
+              en
+              es
+            }
+            slug {
+              current
+            }
+          }
+          slug {
+            current
+          }
+          category {
+            title {
+              en
+              es
+            }
+            slug {
+              current
+            }
+          }
+          description {
+            en
+            es
+          }
+          mainImage {
+            caption
+            alt
+            asset {
+              _id
+            }
+          }
+        }
+      }
+    }`;
+
+    const result = await graphql(postQuery)
+    if (result.errors) {
+      console.log("Error retrieving data from graphql api", result.errors);
+      throw result.errors;
+    }
+
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+
+
+exports.createPages = async ({
+  graphql,
+  actions
+}) => {
+  await createBlogPostPages(graphql, actions)
+}
