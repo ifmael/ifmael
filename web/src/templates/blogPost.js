5e12016bc2357a7e363255bb0eac7daf20473b15
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2020-05-17 13:40:04 +0200

    web(template): Change query and localise post

--- web/src/templates/blogPost.js
+++ web/src/templates/blogPost.js
@@ -1,45 +1,50 @@
 import React from "react"
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
+import localize from "../utils/localize"
 
-
-const BlogPostTemplate = ( { pageContext, data } ) => {
+const BlogPostTemplate = ( { pageContext, data, errrors } ) => {
   const { post} = data
-  const { language } = pageContext
+  const { locale } = pageContext
+  const postLocalised = localize(post, locale)
   return (<Layout>
     {
-      post.title && post.title[language] &&
-        <p> {post.title[language]}</p>
+      postLocalised && postLocalised.title &&
+        <p> {postLocalised.title}</p>
     }
   </Layout>)
 }
 
 
 export const query = graphql`
-  query BlogPostTemplateQuery($id: String!, $es: Boolean!, $en: Boolean!) {
+  query BlogPostTemplateQuery($id: String!) {
     post: sanityPost(id: {eq: $id}){
       title {
-        en @include(if: $en)
-        es @include(if: $es)
+        en
+        es
+        _type
       }
       tags {
         title {
-          en @include(if: $en)
-          es @include(if: $es)
+          en
+          es
+          _type
         }
       }
       category {
         title {
-          en @include(if: $en)
-          es @include(if: $es)
+          en
+          es
+          _type
         }
         slug {
           current
         }
       }
       description {
-        en @include(if: $en)
-        es @include(if: $es)
+        en
+        es
+        _type
       }
       mainImage {
         caption
